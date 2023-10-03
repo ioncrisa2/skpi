bbebf33795fe14feb967a7312bd6eb4405c2be20
ioncrisa2 <zyohanes99@gmail.com>
2023-10-03 14:39:35 +0700

    initial repo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nama_lengkap');
+            $table->string('kelas');
+            $table->string('nis')->unique();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role',['admin','siswa'])->default('siswa');
             $table->rememberToken();
             $table->timestamps();
         });
